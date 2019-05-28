dd5a37b53e8357c38f13a1f5060ebffc0f81337e
Sparrow <1158443037@qq.com>
2019-05-28 16:25:58 +0800

    May 28

---
+++ src/greedyAlgorithm/Num55_JumpGame.java
@@ -0,0 +1,76 @@
+package greedyAlgorithm;
+
+public class Num55_JumpGame {
+	
+	//input:[2,3,1,1,4]
+	//output:true
+	//input:[3,2,1,0,4]
+	//output:false
+	
+	//MyInitialSolution
+	public static boolean canJump(int[] nums) {
+        if(nums == null || nums.length == 0)
+        	return false;
+        int length = nums.length;
+        
+        int cur = 0;
+        int range = nums[cur];
+        int max = range;
+        
+        while(true) {
+        	int temp = cur;
+        	int temp2 = range;
+        	for(int i = 1; i <= temp2 && i < length; i++) {
+        		if(temp+i+nums[temp+i] >= max) {
+        			max = temp+i + nums[temp+i];
+        			cur = temp + i;
+        			range = nums[cur];
+        		}
+        	}
+        	if(nums[cur] == 0 && cur != length - 1)
+        		return false;
+        	if(max >= length - 1)
+        		return true;
+        }
+    }
+	
+	//贪心算法(从后往前跳)
+	public static boolean canJump_greedy(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+	
+	//动态规划   canJump[i] = canJump[i+1]||canJump[i+2]||...canJump[i+nums[i]]
+	public static boolean canJump_DP(int[] nums) {
+		if(nums == null || nums.length == 0)
+			return false;
+		
+		int[] canJump = new int[nums.length];
+		for(int i = nums.length - 1; i >= 0; i--) {
+			if(nums[i] + i >= nums.length - 1)
+				canJump[i] = 1;
+			else {
+				for(int j = 1; j <= nums[i]; j++) {
+					if(canJump[i+j] == 1) {
+						canJump[i] = 1;
+						break;
+					}
+				}
+			}
+		}
+		
+		return canJump[0] == 1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(canJump_DP(new int[] {1,1,2,2,0,1,1}));
+		System.out.println(canJump_DP(new int[] {1}));
+	}
+
+}
---
+++ src/greedyAlgorithm/Num56_MergeIntervals.java
@@ -0,0 +1,74 @@
+package greedyAlgorithm;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Num56_MergeIntervals {
+	
+	//input:[[1,3],[2,6],[8,10],[15,18]]
+	//output:[[1,6],[8,10],[15,18]]
+	//desp:将区间合并
+	
+	//先排序
+	
+	public static int[][] merge(int[][] intervals) {
+		if(intervals == null || intervals.length == 0 || intervals[0].length == 0)
+        	return new int[0][2];
+        
+        List<int[]> l = new LinkedList<int[]>();
+		
+        for(int[] item: intervals)
+        	l.add(item);
+        
+        boolean merged = false;
+        while(true) {
+        	merged = false;
+        	for(int i = 0; i < l.size() - 1; i++) {
+        		for(int j = i + 1; j < l.size(); j++) {
+        			int[] temp = mergeTwo(l.get(i),l.get(j));
+            		if(temp != null) {
+            			l.remove(i);
+            			l.remove(j-1);
+            			l.add(temp);
+            			merged = true;
+            			break;
+            		}
+        		}
+        	}
+        	
+        	if(merged == false)
+        		break;
+        }
+        
+        int[][] result = new int[l.size()][2];
+        for(int i = 0; i < l.size(); i++)
+        	result[i] = l.get(i);
+        
+		return result;
+    }
+	
+	private static int[] mergeTwo(int[] interval1, int[] interval2) {
+		int start1 = interval1[0];
+		int end1 = interval1[1];
+		int start2 = interval2[0];
+		int end2 = interval2[1];
+		
+		if(start1 > end2 || end1 < start2)
+			return null;
+		
+		int[] result = new int[2];
+		result[0] = start1 < start2? start1:start2;
+		result[1] = end1 > end2? end1:end2;
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] temp = new int[][] {new int[] {2,3},
+			new int[] {5,5},new int[] {2,2},new int[] {3,4}
+		,new int[] {3,4}};
+		merge(temp);
+	}
+
+}
